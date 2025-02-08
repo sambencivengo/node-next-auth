a54c37564031172a58133be0102ed9e2e15b6c19
Sam Bencivengo <sambencivengo@gmail.com>
2025-02-08 18:29:11 -0500

    more funky stuff

--- src/queries/users/createUser.ts
+++ src/queries/users/createUser.ts
@@ -1,22 +1,28 @@
-import { db } from "../../db";
-import { users } from "../../db/schema";
+import { db } from "src/db";
+import { users } from "src/db/schema";
+
 
 export const createUser = async () => {
-	try {
-		console.log("Creating user")
-		const newUser: typeof users.$inferInsert = {
-			username: 'John',
-			age: 30,
-			email: 'john@example.com',
-		};
-		const val = await db.insert(users).values(newUser);
+  try {
+    console.log("Creating user")
+    const newUser: typeof users.$inferInsert = {
+      username: 'John',
+      age: 30,
+      email: 'john@example.com',
+    };
+    try {
 
-		console.log("User created!!!!!", { newUser })
-		console.log(val)
-		return { user: newUser };
-	} catch (error: any) {
-		return { error, message: "Unable to create new user" }
-	}
+    } catch (error) {
+
+    }
+    const val = await db.insert(users).values(newUser);
+
+    console.log("User created!!!!!", { newUser })
+    console.log(val)
+    return { user: newUser };
+  } catch (error: any) {
+    return { error, message: "Unable to create new user" }
+  }
 }
 
 
